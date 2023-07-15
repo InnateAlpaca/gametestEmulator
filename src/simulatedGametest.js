15c168dc1be95de409e306d71001fc03ce80323a
InnateAlpaca <105606689+InnateAlpaca@users.noreply.github.com>
2023-07-15 08:43:13 +0000

    Update simulatedGametest.js
    dummy casing change

--- src/simulatedGametest.js
+++ src/simulatedGametest.js
@@ -74,23 +74,23 @@ const HttpRequestMethod = {
     /**
      * Represents the method for an HTTP HEAD request. HEAD requests are similar to a GET request, but are commonly used to retrieve just the HTTP response headers from the specified URI, and not the body contents.
      */
-    DELETE: "DELETE",
+    Delete: "DELETE",
     /**
      * Represents the method for an HTTP PUT request. POST requests are commonly used to create a new resource that is a subordinate of the specified URI.
      */
-    GET: "GET",
+    Get: "GET",
     /**
      * Represents the method for an HTTP PUT request. GET requests are commonly used to retrieve information about a resource at the specified URI.
      */
-    HEAD: "HEAD",
+    Head: "HEAD",
     /**
      * Represents the method for an HTTP PUT request. GET requests are commonly used to retrieve information about a resource at the specified URI.
      */
-    POST: "POST",
+    Post: "POST",
     /**
      * Represents the method for an HTTP PUT request. PUT requests are commonly used to update a single resource that already exists in a resource collection.
      */
-    PUT: "PUT"
+    Put: "PUT"
 }
 
 // ********************************* minecraft/server-net *********************************
